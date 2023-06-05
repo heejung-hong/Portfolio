4675bab29741fe1b05037d1ba2dc612c52d3d0e6
heejung1180 <heejung1180@outlook.com>
2023-06-05 10:17:31 -0700

    JavaScript link for footer social media icon

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+// Event listener for Footer Icons
+// const iconLinks = document.querySelector('#linkicon')
+
+// iconLinks.addEventListener("mouseover", function(e) {
+//     alert('Check out Heejung\'s Linkedin page')
+// })
+
+document.getElementById('giticon').onclick = function() {
+    location.href = "https://github.com/heejung1180";
+};
