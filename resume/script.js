2dedbcd4c68844341c7930428af6a2da3750a141
heejung1180 <heejung1180@outlook.com>
2023-06-05 16:50:02 -0700

    iframe width and height change when clicking button

---
+++ resume/script.js
@@ -0,0 +1,9 @@
+
+// Resume PDF link
+function resume() {
+    let pdf = document.createElement('IFRAME') // Creates html in javascript
+    pdf.setAttribute('src', 'Resume.pdf') 
+    document.body.appendChild(pdf).style.width = '75%'
+    document.body.appendChild(pdf).style.height = '950px'
+};
+
