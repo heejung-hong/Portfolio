a3a469c57117fdb755e03e3a2d2b3c7cc06a6680
heejung1180 <heejung1180@outlook.com>
2023-06-07 22:39:42 -0700

    css properties added

---
+++ about/script.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+// DOM for Footer Icons
+document.getElementById('linkicon').onclick = function() {
+    location.href = "https://www.linkedin.com/in/heejung-hong/";
+  };
+  
+  document.getElementById('giticon').onclick = function() {
+    location.href = "https://github.com/heejung1180";
+  };
+  
